72e61c3b9e9656bdfd2cabd0cad77113084d863d
AelaNieve <dragon27070@gmail.com>
2025-05-30 16:09:14 -0500

    Router servicios y cita bonotes listos

--- frontend/src/views/NewAppointmentsLayout.vue
+++ frontend/src/views/NewAppointmentsLayout.vue
@@ -5,23 +5,35 @@ const route = useRoute()
 </script>
 
 <template>
-  <nav class="my-5 flex gap-3">
-    <RouterLink
-      :to="{ name: 'new-appointment' }"
-      class="flex-1 text-center p-3 uppercase font-extrabold hover:bg-blue-600 hover:text-white"
-      :class="route.name == 'new-appointment' ? 'bg-blue-600 text-white' : 'bg-white text-blue-500'"
-    >
-      Servicios
-    </RouterLink>
-    <RouterLink
-      :to="{ name: 'appointment-details' }"
-      class="flex-1 text-center p-3 uppercase font-extrabold hover:bg-blue-600 hover:text-white"
-      :class="
-        route.name == 'appointment-details' ? 'bg-blue-600 text-white' : 'bg-white text-blue-500'
-      "
-    >
-      Cita y Resumen
-    </RouterLink>
+  <nav class="w-full py-5 px-5 gap-3">
+    <div class="flex space-x-2 rounded-xl bg-teal-600/20 p-1">
+      <RouterLink
+        :to="{ name: 'new-appointment' }"
+        :class="[
+          'w-full rounded-lg py-2.5 text-sm font-extrabold uppercase leading-5 text-center',
+          'ring-white/60 ring-offset-2 ring-offset-teal-400 focus:outline-none focus:ring-2',
+          route.name == 'new-appointment'
+            ? 'bg-white text-teal-700 shadow'
+            : 'text-teal-100 hover:bg-white/[0.12] hover:text-white',
+        ]"
+      >
+        Servicios
+      </RouterLink>
+
+      <RouterLink
+        :to="{ name: 'appointment-details' }"
+        :class="[
+          'w-full rounded-lg py-2.5 text-sm font-extrabold uppercase leading-5 text-center',
+          'ring-white/60 ring-offset-2 ring-offset-teal-400 focus:outline-none focus:ring-2',
+          route.name == 'appointment-details'
+            ? 'bg-white text-teal-700 shadow'
+            : 'text-teal-100 hover:bg-white/[0.12] hover:text-white',
+        ]"
+      >
+        Cita y Resumen
+      </RouterLink>
+    </div>
   </nav>
+
   <RouterView />
 </template>
