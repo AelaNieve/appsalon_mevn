f950b862fc5315313e4326393573261f7b0a8683
AelaNieve <dragon27070@gmail.com>
2025-05-30 19:19:15 -0500

    Pre store citas y servicio funcional al dar click

--- frontend/src/views/ServiceItem.vue
+++ frontend/src/views/ServiceItem.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { formatCurrency } from '../helpers'
 defineProps({
   service: {
     type: Object,
@@ -9,6 +10,6 @@ defineProps({
 <template>
   <div class="p-5 space-y-5 rounded-lg cursor-pointer bg-white">
     <p class="text-2xl font-light">{{ service.name }}</p>
-    <p class="text-2xl font-light">{{ service.price }}</p>
+    <p class="text-4xl font-black text-blue-600">{{ formatCurrency(service.price) }}</p>
   </div>
 </template>
